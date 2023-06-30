788a7a87c41e57af857f30509f8b2c7d775c6f08
Pavel Evstigneev <pro1912@gmail.com>
2023-06-30 15:50:15 +0300

    Apply clearance of bad characters.

--- HeistItemFinder/Realizations/TextFromImageReader.cs
+++ HeistItemFinder/Realizations/TextFromImageReader.cs
@@ -1,5 +1,8 @@
 ﻿using HeistItemFinder.Interfaces;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Text;
+using System.Windows.Documents;
 using Tesseract;
 
 namespace HeistItemFinder.Realizations
@@ -9,6 +12,21 @@ namespace HeistItemFinder.Realizations
         public TextFromImageReader()
         {
         }
+
+        private List<char> _badCharacters = new List<char>()
+        {
+            '_',
+            '-',
+            '—',
+            '!',
+            '*',
+            '#',
+            '<',
+            '>',
+            '$',
+            '%',
+            '@'
+        };
 
         public string GetTextFromImage(Bitmap image)
         {
@@ -23,7 +41,22 @@ namespace HeistItemFinder.Realizations
                     }
                 }
             }
-            return allText;
+            return ClearText(allText);
+        }
+
+        /// <summary>
+        /// Replace all bad characters with space width character.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>Cleared text.</returns>
+        private string ClearText(string text)
+        {
+            var strBuilder = new StringBuilder(text);
+            foreach(var badChar in  _badCharacters)
+            {
+                strBuilder.Replace(badChar, ' ');
+            }
+            return strBuilder.ToString();
         }
     }
 }
