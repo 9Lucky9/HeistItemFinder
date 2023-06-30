9b6595ef7b0785884f7ea58917afd6e01ae4c401
Pavel Evstigneev <pro1912@gmail.com>
2023-06-30 19:51:20 +0300

    Remove redutant field

--- HeistItemFinder/MVVM/Models/LangItem.cs
+++ HeistItemFinder/MVVM/Models/LangItem.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Windows.Media.Imaging;
 
 namespace HeistItemFinder.MVVM.Models
 {
-    internal class LangItem
+    public class LangItem
     {
         public string LanguageCode { get; }
-        public BitmapImage BitmapImage { get; }
         public Uri Uri { get; }
 
         public LangItem(string languageCode)
         {
             LanguageCode = languageCode;
             var uri = new Uri(@"pack://application:,,,/Assets/Language images/" + languageCode + ".png");
-            BitmapImage = new BitmapImage(uri);
             Uri = uri;
         }
     }
