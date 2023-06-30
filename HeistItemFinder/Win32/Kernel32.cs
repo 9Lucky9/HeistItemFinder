c2fa7e5da4193e9a6d8336711ff99d83008d2983
Pavel Evstigneev <pro1912@gmail.com>
2023-06-30 11:20:40 +0300

    Import win api hooks for keyboard hook

---
+++ HeistItemFinder/Win32/Kernel32.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace HeistItemFinder.Win32
+{
+    public class Kernel32
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
+    }
+}
---
+++ HeistItemFinder/Win32/User32.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace HeistItemFinder.Win32
+{
+    public class User32
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int hookType, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, [In] IntPtr lParam);
+
+
+    }
+}
