0e676fb59197fc00c575c1925870cedcba6ee056
Pavel Evstigneev <pro1912@gmail.com>
2023-07-04 12:37:08 +0300

    temp

--- HeistItemFinder.UnitTests/MVVM/ViewModels/SearchViewModelTests.cs
+++ HeistItemFinder.UnitTests/MVVM/ViewModels/SearchViewModelTests.cs
@@ -1,6 +1,17 @@
-﻿namespace HeistItemFinder.UnitTests.MVVM.ViewModels
+﻿using HeistItemFinder.Interfaces;
+using HeistItemFinder.MVVM.Views;
+using Moq;
+
+namespace HeistItemFinder.UnitTests.MVVM.ViewModels
 {
     public class SearchViewModelTests
     {
+        private readonly Mock<IPoeItemsParser> _iPoeNinjaParser;
+        private readonly Mock<IPoeTradeParser> _iPoeTradeParser;
+        private readonly Mock<IOpenCvVision> _iOpenCvVision;
+        private readonly Mock<ITextFromImageReader> _iTextFromImageReader;
+        private readonly Mock<IScreenShotWin32> _iScreenShotWin32;
+        private readonly Mock<IKeyboardHook> _iKeyboardHook;
+        private readonly Popup _popup;
     }
 }
