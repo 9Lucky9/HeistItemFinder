b8d2053ece2b96e24b3053ea013090fae97f916e
Pavel Evstigneev <pro1912@gmail.com>
2023-07-04 23:49:17 +0300

    Add keyword to increase search confidence. Add flow control

--- HeistItemFinder/Realizations/ItemFinder.cs
+++ HeistItemFinder/Realizations/ItemFinder.cs
@@ -1,4 +1,6 @@
-﻿using HeistItemFinder.Models.PoeNinja;
+﻿using HeistItemFinder.Exceptions;
+using HeistItemFinder.Models.PoeNinja;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,58 +14,108 @@ namespace HeistItemFinder.Realizations
         //      that are possible in heist mode.
         //      Keywords could be retrieved from wiki, poe trade etc.
 
+        /// <summary>
+        /// All unique keyword's to find.
+        /// </summary>
+        private static IReadOnlyList<string> _itemKeyWords = new List<string>() 
+        {
+            "divergent",
+            "anomalous",
+            "awakened",
+            "replica",
+            "thief's trinket"
+        };
+
+        /// <summary>
+        /// Find last item in parsed data.
+        /// </summary>
+        /// <param name="equipmentResponse">Parsed items.</param>
+        /// <param name="textFromImage">Text from an image</param>
+        /// <returns>Last listed item with minimal price.</returns>
         public static BaseEquipment FindLastListedItem(
             EquipmentResponse equipmentResponse,
-            string textFromImage,
-            bool applyModificators = false)
+            string textFromImage)
         {
-            var items = equipmentResponse.Lines;
-            var formattedItemName = FormatTextByLines(textFromImage).First();
-
-            if (equipmentResponse.Language is not null)
+            try
             {
-                var translations = equipmentResponse.Language.Translations;
-                var englishName = "";
-                foreach (var translation in translations)
+                var items = equipmentResponse.Lines;
+                foreach(var keyword in _itemKeyWords)
                 {
-                    if (translation.Value.ToLower() == formattedItemName.ToLower())
+                    if(textFromImage.Contains(keyword, StringComparison.OrdinalIgnoreCase))
                     {
-                        englishName = translation.Key;
+                        var ind = textFromImage.IndexOf(keyword, StringComparison.OrdinalIgnoreCase);
+                        textFromImage = textFromImage[ind..];
                         break;
                     }
                 }
-                var lastListedItems = items.Where(
-                    x => x.Name.Contains(englishName, System.StringComparison.OrdinalIgnoreCase));
-                var mininalPriceItem = lastListedItems.MinBy(x => x.ChaosValue);
-                var equipment = new BaseEquipment()
+                var textLines = FormatTextByLines(textFromImage);
+                var formattedItemName = textLines.First();
+
+                if (equipmentResponse.Language is not null)
                 {
-                    Name = formattedItemName,
-                    ChaosValue = mininalPriceItem.ChaosValue,
-                    DivineValue = mininalPriceItem.DivineValue,
-                    Icon = mininalPriceItem.Icon
-                };
-                return equipment;
+                    var translations = equipmentResponse.Language.Translations;
+                    var englishName = "";
+                    foreach (var translation in translations)
+                    {
+                        if (translation.Value.ToLower() == formattedItemName.ToLower())
+                        {
+                            englishName = translation.Key;
+                            break;
+                        }
+                    }
+                    var lastListedItems = items.Where(
+                        x => x.Name.Contains(englishName, StringComparison.OrdinalIgnoreCase));
+                    var mininalPriceItem = lastListedItems.MinBy(x => x.ChaosValue);
+                    var equipment = new BaseEquipment()
+                    {
+                        Name = formattedItemName,
+                        ChaosValue = mininalPriceItem.ChaosValue,
+                        DivineValue = mininalPriceItem.DivineValue,
+                        Icon = mininalPriceItem.Icon
+                    };
+                    return equipment;
+                }
+                else
+                {
+                    var lastListedItems = items.Where(
+                        x => x.Name.Contains(formattedItemName, StringComparison.OrdinalIgnoreCase));
+                    var mininalPriceItem = lastListedItems.MinBy(x => x.ChaosValue);
+                    if(mininalPriceItem is null)
+                    {
+                        throw new ItemNotFoundException(
+                            "Item were not found. Possibly due to lack of item on poe ninja or bad text input.");
+                    }
+                    var equipment = new BaseEquipment()
+                    {
+                        Name = formattedItemName,
+                        ChaosValue = mininalPriceItem.ChaosValue,
+                        DivineValue = mininalPriceItem.DivineValue,
+                        Icon = mininalPriceItem.Icon
+                    };
+                    return equipment;
+                }
             }
-            else
+            catch (NullReferenceException)
             {
-                var lastListedItems = items.Where(
-                    x => x.Name.Contains(formattedItemName, System.StringComparison.OrdinalIgnoreCase));
-                var mininalPriceItem = lastListedItems.MinBy(x => x.ChaosValue);
-                var equipment = new BaseEquipment()
-                {
-                    Name = formattedItemName,
-                    ChaosValue = mininalPriceItem.ChaosValue,
-                    DivineValue = mininalPriceItem.DivineValue,
-                    Icon = mininalPriceItem.Icon
-                };
-                return equipment;
+                throw new ItemNotFoundException(
+                    "Item were not found. Possibly due to lack of item on poe ninja or bad text input.");
             }
+            catch(Exception) 
+            {
+                throw new ItemNotFoundException(
+                    "Item were not found. Possibly due to lack of item on poe ninja or bad text input.");
+            }
+
         }
 
 
+        /// <summary>
+        /// Format text as lines.
+        /// </summary>
+        /// <returns>List of text lines.</returns>
         private static List<string> FormatTextByLines(string text)
         {
-            List<string> lines = new List<string>();
+            var lines = new List<string>();
             using (var sr = new StringReader(text))
             {
                 string line;
