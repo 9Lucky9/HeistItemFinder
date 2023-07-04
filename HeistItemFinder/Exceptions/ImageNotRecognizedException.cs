1583e641a233c55b64237c7e5b41442ca465ddb3
Pavel Evstigneev <pro1912@gmail.com>
2023-07-04 23:46:29 +0300

    Add new exceptions, rename existing

---
+++ HeistItemFinder/Exceptions/ImageNotRecognizedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HeistItemFinder.Exceptions
+{
+    /// <summary>
+    /// Represent image proccessing issues.
+    /// Bad image source or bad image processing alghoritm.
+    /// </summary>
+    public class ImageNotRecognizedException : Exception
+    {
+        public ImageNotRecognizedException(string? message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ HeistItemFinder/Exceptions/NoTemplateMatchesException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HeistItemFinder.Exceptions
+{
+    /// <summary>
+    /// Represent lack of templates on the source image.
+    /// Often failure due to bad angle of the screenshot or
+    /// artfacts on screenshot.
+    /// Solution: try again.
+    /// </summary>
+    public class NoTemplateMatchesException : Exception
+    {
+        public NoTemplateMatchesException(string? message) : base(message)
+        {
+        }
+    }
+}
