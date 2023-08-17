bca77f05fc5d6ba709dc0fff687ad99d8df4fa46
Pavel Evstigneev <pro1912@gmail.com>
2023-08-17 05:19:11 +0300

    Add leagues parser

---
+++ HeistItemFinder/Interfaces/ILeaguesParser.cs
@@ -0,0 +1,11 @@
+﻿using HeistItemFinder.Models.PoeNinja;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HeistItemFinder.Interfaces
+{
+    public interface ILeaguesParser
+    {
+        public Task<List<EconomyLeague>> GetCurrentLeagues();
+    }
+}
---
+++ HeistItemFinder/Realizations/LeaguesParser.cs
@@ -0,0 +1,24 @@
+﻿using HeistItemFinder.Data;
+using HeistItemFinder.Interfaces;
+using HeistItemFinder.Models.PoeNinja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace HeistItemFinder.Realizations
+{
+    public class LeaguesParser : ILeaguesParser
+    {
+        public async Task<List<EconomyLeague>> GetCurrentLeagues()
+        {
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(PoeNinjaUrls.BaseApiUrl);
+            var response = await httpClient.GetAsync(PoeNinjaUrls.LeaguesRequestUrl);
+            var leagues = await response.Content.ReadFromJsonAsync<LeagueResponse>();
+            return leagues.EconomyLeagues.ToList();
+        }
+    }
+}
