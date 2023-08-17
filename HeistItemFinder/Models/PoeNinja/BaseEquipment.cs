ba9b58f043b22b08f919dd349604d738c553385a
Pavel Evstigneev <pro1912@gmail.com>
2023-08-17 05:29:18 +0300

    new poe ninja model

--- HeistItemFinder/Models/PoeNinja/BaseEquipment.cs
+++ HeistItemFinder/Models/PoeNinja/BaseEquipment.cs
@@ -45,9 +45,11 @@
 
     public class ExplicitModifier
     {
-        public ExplicitModifier() { }
+        public string TradeId { get; set; }
+        public int Value { get; set; }
         public string Text { get; set; }
         public bool Optional { get; set; }
+        public ExplicitModifier() { }
     }
 
 }
