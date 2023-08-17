3f03c6d512d8af7244f2bedb573ef84f6325b835
Pavel Evstigneev <pro1912@gmail.com>
2023-08-17 05:21:02 +0300

    add static urls storage

---
+++ HeistItemFinder/Data/PoeNinjaUrls.cs
@@ -0,0 +1,15 @@
+﻿namespace HeistItemFinder.Data
+{
+    public static class PoeNinjaUrls
+    {
+        public static readonly string BaseApiUrl = 
+            "https://poe.ninja/api/data/";
+
+        public static readonly string ItemsRequestTemplate = 
+            "itemoverview?league={0}&type={1}&language={2}";
+
+        public static readonly string LeaguesRequestUrl =
+            "getindexstate?";
+
+    }
+}
