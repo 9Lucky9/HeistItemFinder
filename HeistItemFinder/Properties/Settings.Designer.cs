4df481a7537869e5c16ffd18052484eddbdd0b70
Pavel Evstigneev <pro1912@gmail.com>
2023-08-17 05:22:19 +0300

    Add new settings to store selected league

--- HeistItemFinder/Properties/Settings.Designer.cs
+++ HeistItemFinder/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HeistItemFinder.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -67,6 +67,18 @@ namespace HeistItemFinder.Properties {
             }
             set {
                 this["SearchKeysCombination"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Crucible")]
+        public string SelectedLeague {
+            get {
+                return ((string)(this["SelectedLeague"]));
+            }
+            set {
+                this["SelectedLeague"] = value;
             }
         }
     }
